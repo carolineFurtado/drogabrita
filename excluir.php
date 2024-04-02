f3b716e405c9d7d2d0eca9cc0e054670e7e677a7
Caroline Furtado <75492974+carolineFurtado@users.noreply.github.com>
2024-04-02 12:12:18 -0300

    Add files via upload

---
+++ excluir.php
@@ -0,0 +1,17 @@
+<?php
+
+	require '../include/conectar.php';
+
+	$id = $_GET["id"];
+	
+	$sql = "delete from remedios where id = $id";
+
+	
+			
+	$result = mysqli_query($connection, $sql);
+	
+	mysqli_close($connection);
+		
+	header('Location: pesquisarAdmin.php');
+
+?>
